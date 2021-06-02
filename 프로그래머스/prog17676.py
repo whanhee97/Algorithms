695ed0b8542eb977a45c7bf66f4f839535536dc1
hhlee <whanhee97@naver.com>
2021-06-02 15:53:51 +0900

    Add: lv.3 1문제

---
+++ 프로그래머스/prog17676.py
@@ -0,0 +1,35 @@
+# 프로그래머스 - 추석 트래픽
+# https://programmers.co.kr/learn/courses/30/lessons/17676
+
+def is_include(start, line):
+    if line[0] > round(start+0.999, 3) or line[1] < start:
+        return False
+    else:
+        return True
+
+
+def solution(lines):
+    answer = 0
+    startNfinish = []
+    for line in lines:
+        arr = line.split(' ')
+        res_time = arr[1].split(':')
+        res_sec_f = float(res_time[0])*3600 + \
+            float(res_time[1])*60 + float(res_time[2])
+        res_sec_s = round(res_sec_f - float(arr[2][:-1]) + 0.001, 3)
+        startNfinish.append([res_sec_s, res_sec_f])
+    startNfinish.sort(key=lambda x: x[1])
+    for i in range(len(startNfinish)):
+        for j in range(2):
+            cnt = 0
+            start = startNfinish[i][j]
+            for k in startNfinish:
+                if is_include(start, k):
+                    cnt += 1
+            if answer <= cnt:
+                answer = cnt
+    return answer
+
+
+lines = ["2016-09-15 01:00:04.002 2.0s", "2016-09-15 01:00:07.000 2s"]
+print(solution(lines))
