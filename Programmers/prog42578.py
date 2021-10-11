a51d58606916161f0c034265cdc3532e3332d231
hhlee <whanhee97@naver.com>
2021-10-11 18:12:21 +0900

    Add: hash problem (42578)

---
+++ Programmers/prog42578.py
@@ -0,0 +1,16 @@
+def solution(clothes):
+    answer = 1
+    c_dict = dict()
+    for c in clothes:
+        if c[1] not in c_dict:
+            c_dict[c[1]] = []
+        c_dict[c[1]].append(c[0])
+    for k in c_dict.keys():
+        answer *= (len(c_dict[k])+1)
+    print(answer)
+    return answer
+
+
+clothes = [["yellowhat", "headgear"], [
+    "bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
+solution(clothes)
