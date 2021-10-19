da87e8e91eae70e7dc7eb4c10830d2db094b2ada
hhlee <whanhee97@naver.com>
2021-10-19 17:04:25 +0900

    Modified: 조이스틱

--- Greedy/prog42860.py
+++ Greedy/prog42860.py
@@ -7,25 +7,27 @@ def solution(name):
     name = list(name)
     upDown = []
     for i in name:
-        upDown.append(min(ord(i)-ord('A'), ord('Z')-ord(i)+1)) #name의 각 자리에 up down 수를 구해서 적어놓음(+1 해주는 이유는 첨 상태에서 왼쪽으로 가는 횟수 추가)
+        # name의 각 자리에 up down 수를 구해서 적어놓음(+1 해주는 이유는 첨 상태에서 왼쪽으로 가는 횟수 추가)
+        upDown.append(min(ord(i)-ord('A'), ord('Z')-ord(i)+1))
     idx = 0
     while True:
-        answer += upDown[idx] # 인덱스에 해당하는 숫자 더해주고
-        upDown[idx] = 0 # 그 자리 0으로 만듬
-        if sum(upDown) == 0: # 모든 자리 합이 0이 되면 종료
+        answer += upDown[idx]  # 인덱스에 해당하는 숫자 더해주고
+        upDown[idx] = 0  # 그 자리 0으로 만듬
+        if sum(upDown) == 0:  # 모든 자리 합이 0이 되면 종료
             break
-        right = 1 # 한 칸씩 이동하는 것은 고정
+        right = 1  # 한 칸씩 이동하는 것은 고정
         left = 1
-        while upDown[idx+right] == 0: # 오른쪽으로 이동한게 0이면 오른쪽으로 한 칸 이동 -> A가 아닌 것 까지의 거리를 오른쪽으로 잰다
+        while upDown[idx+right] == 0:  # 오른쪽으로 이동한게 0이면 오른쪽으로 한 칸 이동 -> A가 아닌 것 까지의 거리를 오른쪽으로 잰다
             right += 1
-        while upDown[idx-left] == 0: # 왼쪽으로 이동한게 0이면 왼쪽으로 한 칸 이동 -> A가 아닌 것 까지의 거리를 왼쪽으로 잰다
+        while upDown[idx-left] == 0:  # 왼쪽으로 이동한게 0이면 왼쪽으로 한 칸 이동 -> A가 아닌 것 까지의 거리를 왼쪽으로 잰다
             left += 1
-        if right <= left: # 더 작은 쪽으로 이동함
-            answer += right # 이동한 거리 추가
-            idx += right # idx 실제 이동
+        if right <= left:  # 더 작은 쪽으로 이동함
+            answer += right  # 이동한 거리 추가
+            idx += right  # idx 실제 이동
         else:
-            answer += left # 이동한 거리 추가
-            idx -= left # idx 실제 이동
+            answer += left  # 이동한 거리 추가
+            idx -= left  # idx 실제 이동
     return answer
+
 
 print(solution("JAN"))
