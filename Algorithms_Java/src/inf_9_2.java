f00d36e43bed6b2a87bda948715be110bcfd18e3
hhlee <whanhee97@naver.com>
2024-08-20 20:48:32 +0900

    first commit

--- Algorithms_Java/src/inf_9_2.java
+++ Algorithms_Java/src/inf_9_2.java
@@ -1,14 +1,28 @@
 import java.util.*;
-// Time Limit Exceeded
+
+class Time implements Comparable<Time>{
+    public int s,e;
+    public Time(int s, int e){
+        this.s = s;
+        this.e = e;
+    }
+
+    @Override
+    public int compareTo(Time t){
+        return this.e != t.e?this.e - t.e : this.s - t.s;
+    }
+}
+
 public class inf_9_2 {
 
-    public static int solution(int n, ArrayList<int[]> times){
-        Collections.sort(times, (a,b)-> a[1] != b[1] ? a[1]-b[1] : a[0]-b[0]);
+
+    public static int solution(int n, ArrayList<Time> times){
+        Collections.sort(times);
         int eoc = 0;
         int cnt = 0;
-        for(int[] x : times){
-            int s = x[0];
-            int e = x[1];
+        for(Time x : times){
+            int s = x.s;
+            int e = x.e;
             if(eoc <= s){
                 eoc = e;
                 cnt++;
@@ -18,12 +32,13 @@ public class inf_9_2 {
     }
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
+
         int n = kb.nextInt();
-        ArrayList<int[]> times = new ArrayList<>();
+        ArrayList<Time> times = new ArrayList<>();
         for(int i=0;i<n;i++){
             int s = kb.nextInt();
             int e = kb.nextInt();
-            times.add(new int[]{s,e});
+            times.add(new Time(s,e));
         }
 
         System.out.print(solution(n,times));
