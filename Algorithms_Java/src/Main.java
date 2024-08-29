675b9cc6a0d53e4a005f9d04a97cb42e93750809
hhlee <whanhee97@naver.com>
2024-08-29 20:50:05 +0900

    Update Main.java

--- Algorithms_Java/src/Main.java
+++ Algorithms_Java/src/Main.java
@@ -14,14 +14,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        int[][] arr = new int[n][2];
-        for(int i =0;i<n;i++){
+        int m = kb.nextInt();
+
+        int[][] arr = new int[m][2];
+        for(int i =0;i<m;i++){
             arr[i][0] = kb.nextInt();
             arr[i][1] = kb.nextInt();
         }
+        int a = kb.nextInt();
+        int b = kb.nextInt();
 
-        inf_9_4 T = new inf_9_4();
+        inf_9_6 T = new inf_9_6();
 
-        System.out.print(T.solution(n,arr));
+        System.out.print(T.solution(n,m,arr,a,b));
     }
 }
