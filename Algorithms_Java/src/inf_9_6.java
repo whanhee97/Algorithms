b0b8d8d092b0a12c1ae72cd8a030155eca6f7e32
hhlee <whanhee97@naver.com>
2024-08-29 20:49:48 +0900

    add:inf_9_6

---
+++ Algorithms_Java/src/inf_9_6.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class inf_9_6 {
+    static int[] parents;
+
+    public int find(int t){
+        if(parents[t] == t){
+            return t;
+        }else{
+            int parent = find(parents[t]);
+            return parent;
+        }
+    }
+
+    public void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a != b)
+            parents[b] = a;
+    }
+
+    public String solution(int n, int m, int[][] arr, int a, int b){
+        String answer;
+        parents = new int[n+1];
+
+        for(int i=1;i<=n;i++){
+            parents[i] = i;
+        }
+
+        for(int[] coup: arr){
+            union(coup[0],coup[1]);
+        }
+        if(find(a) == find(b)) answer = "YES";
+        else answer = "NO";
+
+        return answer;
+    }
+}
