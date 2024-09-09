76e82c33f644271e5e148ceb605d71f83d7b31d4
hhlee <whanhee97@naver.com>
2024-09-09 22:13:31 +0900

    add:inf_10_2

---
+++ Algorithms_Java/src/inf_10_2.java
@@ -0,0 +1,11 @@
+public class inf_10_2 {
+    public int solution(int n){
+        int[] dp = new int[n+2];
+        dp[0]= 1;
+        dp[1]= 1;
+        for(int i=2;i<=n+1;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n+1];
+    }
+}
